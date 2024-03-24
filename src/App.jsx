125460b5e9e0c274978dd1f128947242942522a5
*Muhammad <*idreesnawab78@gmail.com*>
2024-03-24 11:41:24 -0700

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,42 @@ import { useState } from 'react';
 import './index.css';
 
 const App = () => {
-  const [Color, setColor] = useState('Olive');
-
-  const colors = [
-    { name: 'Green', color: 'green' },
-    { name: 'Blue', color: 'blue' },
-    { name: 'Red', color: 'red' },
-    { name: 'Yellow', color: 'yellow' },
-  ];
+  const [color, setColor] = useState('Olive');
 
   return (
-    <div className='h-screen w-full flex items-center justify-center gap-4 transition-colors duration-300' style={{ backgroundColor: Color }}>
-      {colors.map((colorObj) => (
-        <button
-          key={colorObj.name}
-          className={`bg-${colorObj.color}-600 rounded p-[1rem] text-white outline-none`}
-          onClick={() => {
-            setColor(colorObj.color);
-          }}
-        >
-          {colorObj.name}
-        </button>
-      ))}
+    <div className='h-screen w-full flex flex-wrap items-center justify-center gap-4 transition-colors duration-300' style={{ backgroundColor: color }}>
+      <button
+        className='bg-green-600 rounded p-[1rem] text-white outline-none'
+        onClick={() => {
+          setColor('green');
+        }}
+      >
+        Green
+      </button>
+      <button
+        className='bg-blue-600 rounded p-[1rem] text-white outline-none'
+        onClick={() => {
+          setColor('blue');
+        }}
+      >
+        Blue
+      </button>
+      <button
+        className='bg-red-600 rounded p-[1rem] text-white outline-none'
+        onClick={() => {
+          setColor('red');
+        }}
+      >
+        Red
+      </button>
+      <button
+        className='bg-yellow-600 rounded p-[1rem] text-white outline-none'
+        onClick={() => {
+          setColor('yellow');
+        }}
+      >
+        Yellow
+      </button>
     </div>
   );
 };
